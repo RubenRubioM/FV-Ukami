439713601c95f0bab8439d6f2ab4b99a205a7659
RUBÉN RUBIO MARTÍNEZ <rrm73@clL16-3>
2019-03-06 14:28:34 +0100

    Commit inicial

---
+++ Ukami/src/Game.cpp
@@ -0,0 +1,29 @@
+
+#ifndef GAME_H
+#define GAME_H
+
+#include "SFML/Graphics.hpp"
+#include <vector>
+
+using namespace std;
+
+
+class Game
+{
+    public:
+        Game(int x,int y, std::string title);
+        void draw();
+        void gameLoop();
+        void eventsLoop();
+
+        virtual ~Game();
+
+    protected:
+
+    private:
+
+        sf::RenderWindow *window;
+        sf::Event *event;
+};
+
+#endif // GAME_H
