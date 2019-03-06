a50a43b797a8dab764414a0826d6280bbe669316
RubenRubioM <rubensipala@gmail.com>
2019-03-06 16:27:25 +0100

    Bucle maestro

--- Ukami/include/Game.h
+++ Ukami/include/Game.h
@@ -1,14 +1,28 @@
+
 #ifndef GAME_H
 #define GAME_H
+
+#include "SFML/Graphics.hpp"
+
+using namespace std;
 
 
 class Game
 {
     public:
-        Game();
+        Game(int x,int y, std::string title);
+        void draw();
+        void gameLoop();
+        void eventsLoop();
+
         virtual ~Game();
+
     protected:
+
     private:
+
+        sf::RenderWindow *window;
+        sf::Event *event;
 };
 
 #endif // GAME_H
--- Ukami/src/Game.cpp
+++ Ukami/src/Game.cpp
@@ -1,29 +1,112 @@
 
-#ifndef GAME_H
-#define GAME_H
+#include "Game.h"
 
-#include "SFML/Graphics.hpp"
-#include <vector>
+#include <iostream>
+#include <math.h>
+
+
 
 using namespace std;
 
-
-class Game
+Game::Game(int x, int y, string title)
 {
-    public:
-        Game(int x,int y, std::string title);
-        void draw();
-        void gameLoop();
-        void eventsLoop();
 
-        virtual ~Game();
+    window = new sf::RenderWindow(sf::VideoMode(x,y),title);
+    event = new sf::Event();
 
-    protected:
+    //GameLoop, infinity iterations
+    gameLoop();
+}
 
-    private:
+void Game::draw(){
+    window->clear();
 
-        sf::RenderWindow *window;
-        sf::Event *event;
-};
+    //Dibujamos todos los sprites
 
-#endif // GAME_H
+
+    window->display();
+}
+
+//Function that executes once per frame
+void Game::gameLoop(){
+
+    while(window->isOpen()){
+        eventsLoop();
+        draw();
+    }
+}
+
+void Game::eventsLoop(){
+
+    while(window->pollEvent(*event)){
+
+        switch(event->type){
+
+            //Presiona la tecla
+            case sf::Event::KeyPressed:
+                switch(event->key.code){
+
+                    case sf::Keyboard::Escape:
+
+                        window->close();
+                        break;
+
+                    case sf::Keyboard::W: //Presiona la W
+
+                        break;
+
+                    case sf::Keyboard::D: //Presiona la D
+                        cout << "ASDASD";
+                        break;
+
+                    case sf::Keyboard::A: //Presiona la A
+
+                        break;
+
+                    default:
+                        break;
+
+
+                }//End KeyPressed event
+                break;
+
+
+            //Levanta la tecla
+            case sf::Event::KeyReleased:
+                switch(event->key.code){
+
+                    case sf::Keyboard::D:
+
+                        break;
+
+                    case sf::Keyboard::A:
+
+                        break;
+                    case sf::Keyboard::W:
+
+                        break;
+
+                }//End KeyReleased event
+                break;
+
+            //Hit close button
+            case sf::Event::Closed:
+
+                window->close();
+                break;
+
+
+
+        }//End switch event type
+
+
+    }//End events loop
+
+
+}
+
+
+Game::~Game()
+{
+    //dtor
+}
