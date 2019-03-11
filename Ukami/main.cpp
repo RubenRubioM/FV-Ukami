c4ecca1763ff46a5776de0d94927dbcc47f4564a
TheD00mslayer <44227355+TheD00mslayer@users.noreply.github.com>
2019-03-11 09:25:03 +0100

    Animacion del personaje

--- Ukami/main.cpp
+++ Ukami/main.cpp
@@ -1,15 +1,106 @@
 #include <SFML/Graphics.hpp>
+#include "AnimationSprite.h"
 #include <iostream>
-
-#include "kanji.h"
-
-using namespace sf;
 
 int main()
 {
+    // setup window
+    sf::Vector2i screenDimensions(800,600);
+    sf::RenderWindow window(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Movement animation");
+    window.setFramerateLimit(60);
 
-    kanji* puzzle;
-    puzzle = new kanji(1080, 720, 0, "Puzzle Kanji");
+    // load texture (spritesheet)
+    sf::Texture texture;
+    if (!texture.loadFromFile("right.png"))
+    {
+        std::cout << "Failed to load player spritesheet!" << std::endl;
+        return 1;
+    }
 
-    return EXIT_SUCCESS;
+    sf::Texture texture1;
+
+        if (!texture1.loadFromFile("left.png"))
+    {
+        std::cout << "Failed to load player spritesheet!" << std::endl;
+        return 1;
+    }
+
+
+    // set up the animations for all four directions (set spritesheet and push frames)
+    Animation walkingAnimationLeft;
+    walkingAnimationLeft.setSpriteSheet(texture1);
+    walkingAnimationLeft.addFrame(sf::IntRect(750, 0, 150, 90));
+    walkingAnimationLeft.addFrame(sf::IntRect(600, 0, 150, 90));
+    walkingAnimationLeft.addFrame(sf::IntRect(450, 0, 150, 90));
+    walkingAnimationLeft.addFrame(sf::IntRect(300, 0, 150, 90));
+    walkingAnimationLeft.addFrame(sf::IntRect(150, 0, 150, 90));
+    walkingAnimationLeft.addFrame(sf::IntRect(0, 0, 150, 90));
+
+    Animation walkingAnimationRight;
+    walkingAnimationRight.setSpriteSheet(texture);
+    walkingAnimationRight.addFrame(sf::IntRect(0, 0, 150, 90));
+    walkingAnimationRight.addFrame(sf::IntRect(150, 0, 150, 90));
+    walkingAnimationRight.addFrame(sf::IntRect(300, 0, 150, 90));
+    walkingAnimationRight.addFrame(sf::IntRect(450, 0, 150, 90));
+    walkingAnimationRight.addFrame(sf::IntRect(600, 0, 150, 90));
+    walkingAnimationRight.addFrame(sf::IntRect(750, 0, 150, 90));
+
+    Animation* currentAnimation = &walkingAnimationRight;
+
+    // set up AnimatedSprite
+    AnimatedSprite animatedSprite(sf::seconds(0.2), true, false);
+    animatedSprite.setPosition(sf::Vector2f(screenDimensions / 2));
+
+    sf::Clock frameClock;
+
+    float speed = 80.f;
+    bool noKeyWasPressed = true;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+                window.close();
+        }
+
+        sf::Time frameTime = frameClock.restart();
+
+        // if a key was pressed set the correct animation and move correctly
+        sf::Vector2f movement(0.f, 0.f);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            currentAnimation = &walkingAnimationLeft;
+            movement.x -= speed;
+            noKeyWasPressed = false;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            currentAnimation = &walkingAnimationRight;
+            movement.x += speed;
+            noKeyWasPressed = false;
+        }
+        animatedSprite.play(*currentAnimation);
+        animatedSprite.move(movement * frameTime.asSeconds());
+
+        // if no key was pressed stop the animation
+        if (noKeyWasPressed)
+        {
+            animatedSprite.stop();
+        }
+        noKeyWasPressed = true;
+
+        // update AnimatedSprite
+        animatedSprite.update(frameTime);
+
+        // draw
+        window.clear();
+        window.draw(animatedSprite);
+        window.display();
+    }
+
+    return 0;
 }
