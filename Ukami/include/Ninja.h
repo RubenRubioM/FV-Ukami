23d094412964917429ef3ebb0f43a5217894d68d
Ruben <rubensipala@gmail.com>
2019-03-29 09:32:30 +0100

    committttttt

--- Ukami/include/Ninja.h
+++ Ukami/include/Ninja.h
@@ -1,41 +1,33 @@
 #ifndef NINJA_H
 #define NINJA_H
 
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
+#include "tinyxml2.h"
+#include "TextureHolder.h"
+#include "Map.h"
+#include "Box2D/Box2D.h";
 #include <iostream>
+#include <vector>
+#include <map>
+
+using namespace tinyxml2;
 using namespace std;
+using namespace sf;
+
 class Ninja
 {
     public:
         Ninja();
         virtual ~Ninja();
-
-
-        //Getters
-        int getDireccion();
+        void updateMovement(View &view);
+        void drawNinja(RenderWindow &window);
         bool getMoviendose();
-        float getVelocidad();
-        sf::Sprite *getSprite();
-
-        //Setters
-        void setDireccion(int);
-        void setMoviendose(bool);
-
-        //Metodos
-        void moverse(float);
-
-
     protected:
-
-        //Varialbes
-        int direccion = 1; //1-> derecha / -1-> izquierda
-        bool moviendose =false; //Dice si se esta moviendo o no
-        float velocidad;
-        sf::Texture *texture;
-        sf::Sprite *sprite;
-
-        //Metodos
-
+        Texture texture;
+        Sprite sprite;
+        b2Body *ninjaBody;
+        bool moviendose;
+    private:
 
 };
 
--- Ukami/include/Ninja2.h
+++ Ukami/include/Ninja2.h
@@ -1,27 +1,22 @@
 #ifndef NINJA2_H
 #define NINJA2_H
 
-#include <Ninja.h>
-
+#include "Ninja.h"
 
 class Ninja2 : public Ninja
 {
     public:
-        static Ninja2 *getInstance(){
-            if(unicaInstancia==0){
-                unicaInstancia = new Ninja2();
-            }
-            return unicaInstancia;
-        }
+        Ninja2(float posx, float posy, b2World* world);
         virtual ~Ninja2();
-
-
+        void updateMovement(View &view);
+        void drawNinja(RenderWindow &window);
+        Sprite getSprite();
+    protected:
 
     private:
-        Ninja2();
-        static Ninja2 *unicaInstancia;
-
-    protected:
+        Texture texture;
+        Sprite sprite;
+        b2Body *ninjaBody;
 };
 
 #endif // NINJA2_H
